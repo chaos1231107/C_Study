677b23b0e2b8455f4b814ee71b842fc3c232bce0
박건우 <136959177+chaos1231107@users.noreply.github.com>
2023-08-09 23:32:44 +0900

    Add files via upload

--- month_31_pointer.cpp
+++ month_31_pointer.cpp
@@ -1,28 +1,26 @@
 #include <stdio.h>
 
-int find_month(int *ary);
-void change_29(int *ary_day);
-
+int find_month(int *ary, int size1); // 배열의 길이를 구하고자 size 매개변수 사용
+void change_29(int *ary_day, int size2);
 
 int main(void)
 {
     int temp1;
-    //int cnt;
-
     int month[12] = {31, 28, 31, 30, 31, 30,31,31,30,31,30,31};
+    int size = sizeof(month) / sizeof(*month); //배열의 길이
     // 함수 호출
-    temp1 = find_month(month);
-    change_29(month);
+    temp1 = find_month(month,size);
+    change_29(month,size);
     printf("\n");
     printf("31일인 월의 개수는 %d개", temp1);
     return 0;
 
 }
 
-int find_month(int *ary)
+int find_month(int *ary, int size1)
 {
     int cnt;
-    for(int i = 0; i < 12; i++)
+    for(int i = 0; i < size1; i++)
     {
         if(*(ary+i) == 31)  cnt++; // i번째 배열 요소가 31이면 cnt 1씩 증가
     }
@@ -30,10 +28,10 @@ int find_month(int *ary)
     return cnt; // cnt 반환
 }
 
-void change_29(int *ary_day)
+void change_29(int *ary_day,int size2)
 {
     *(ary_day+1) = 29; //배열의 첫번째 요소값을 29로 갱신
-    for(int i = 0; i < 12; i++)
+    for(int i = 0; i < size2; i++)
     {
         printf("%5d", *(ary_day+i)); // 배열요소 출력
     }
